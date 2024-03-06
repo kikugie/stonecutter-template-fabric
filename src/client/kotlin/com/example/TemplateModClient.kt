581664c6eb6e1bae4aedccad8ca49d003c3db63d
Kikugie <kikugie@duck.com>
2024-03-06 11:25:44 +0100

    other(template): uh remove stuffs

--- src/client/kotlin/com/example/TemplateModClient.kt
+++
@@ -1,9 +0,0 @@
-package com.example
-
-import net.fabricmc.api.ClientModInitializer
-
-object TemplateModClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
--- src/main/kotlin/com/example/TemplateModDataGenerator.kt
+++
@@ -1,9 +0,0 @@
-package com.example
-
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
-
-object TemplateModDataGenerator : DataGeneratorEntrypoint {
-	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
-	}
-}
