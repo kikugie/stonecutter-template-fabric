ab6b56e243401c7ed61b86fbb494c29d58786bae
Kikugie <kikugie@duck.com>
2024-03-06 10:59:23 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/TemplateMod.kt
@@ -0,0 +1,15 @@
+package com.example
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object TemplateMod : ModInitializer {
+    private val logger = LoggerFactory.getLogger("template")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
---
+++ src/main/kotlin/com/example/TemplateModDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.example
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object TemplateModDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
