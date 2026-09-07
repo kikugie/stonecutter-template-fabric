a7f36fb094b36368b877507964e0806e3a391535
KikuGie <git.kikugie@protonmail.com>
2026-09-07 13:57:35 +0200

    Update `TemplateMod.java`

--- src/main/java/com/example/TemplateMod.java
+++ src/main/java/com/example/TemplateMod.java
@@ -11,7 +11,7 @@ public class TemplateMod implements ModInitializer {
     // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("template");
     public static final String VERSION = /*$ mod_version*/ "0.1.0";
-    public static final String MINECRAFT = /*$ minecraft*/ "26.1.2";
+    public static final String MINECRAFT = /*$ minecraft*/ "26.2";
 
     @Override
     public void onInitialize() {
@@ -21,9 +21,6 @@ public class TemplateMod implements ModInitializer {
 
         //? if !release
         LOGGER.info("Hello Fabric world!");
-
-        LOGGER.warn("I'm still a template!");
-        LOGGER.warn("I'm still a template!");
     }
 
     /**
